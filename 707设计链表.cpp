02daecc3bf15c843bb6961b9fe0762dea2fb4ec5
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-02-23 23:20:45 +0800

    Create 707设计链表.cpp

---
+++ 707设计链表.cpp
@@ -0,0 +1,80 @@
+class MyLinkedList {
+public:
+    // 定义链表节点结构体
+    struct LinkedNode {
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val):val(val), next(nullptr){}
+    };
+
+    // 初始化链表
+    MyLinkedList() {
+        _dummyHead = new LinkedNode(0); 
+        _size = 0;
+    }
+
+ 
+    int get(int index) {
+        if (index > (_size - 1) || index < 0) {
+            return -1;
+        }
+        LinkedNode* cur = _dummyHead->next;
+        while(index--){ // 如果--index 就会陷入死循环
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+
+
+    void addAtHead(int val) {
+        LinkedNode* newNode = new LinkedNode(val);
+        newNode->next = _dummyHead->next;
+        _dummyHead->next = newNode;
+        _size++;
+    }
+
+
+    void addAtTail(int val) {
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(cur->next != nullptr){
+            cur = cur->next;
+        }
+        cur->next = newNode;
+        _size++;
+    }
+
+
+    void addAtIndex(int index, int val) {
+
+        if(index > _size) return;
+        if(index < 0) index = 0;        
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+        _size++;
+    }
+
+    void deleteAtIndex(int index) {
+        if (index >= _size || index < 0) {
+            return;
+        }
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur ->next;
+        }
+        LinkedNode* tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+        _size--;
+    }
+
+private:
+    int _size;
+    LinkedNode* _dummyHead;
+
+};
