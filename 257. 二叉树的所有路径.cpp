142c404b2a5a225c8809296ad195caa224830848
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-04-05 15:22:46 +0800

    Create 257. 二叉树的所有路径.cpp

---
+++ 257. 二叉树的所有路径.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void traversal(TreeNode* cur,vector<int>& path,vector<string>& result)
+    {
+        path.push_back(cur->val);
+        if(cur->left == NULL && cur->right ==NULL)
+        {
+            string spath;
+            for(int i = 0;i < path.size() - 1;i++)
+            {
+                spath+=to_string(path[i]);
+                spath += "->";
+            }
+            spath += to_string(path[path.size() - 1]);
+            result.push_back(spath);
+            return;
+        }
+        if(cur->left)
+        {
+            traversal(cur->left,path,result);
+            path.pop_back();
+        }
+        if(cur->right)
+        {
+            traversal(cur->right,path,result);
+            path.pop_back();
+        }
+
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        vector<int> path;
+        if(root == NULL) return result;
+        traversal(root,path,result);
+        return result;
+    }
+};
