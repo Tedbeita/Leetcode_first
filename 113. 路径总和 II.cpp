444dbcb22da0d878a3c087eb3467841b6dfb6433
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-04-09 14:32:06 +0800

    Create 113. 路径总和 II.cpp

---
+++ 113. 路径总和 II.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    void traversal(TreeNode* cur,int count)
+    {
+        if(!cur->left && !cur->right && count == 0)
+        {
+            result.push_back(path);
+            return;
+        }
+        if(!cur->left && !cur->right)
+        {
+            return;
+        }
+        
+        if(cur->left)
+        {
+            path.push_back(cur->left->val);
+            count -= cur->left->val;
+            traversal(cur->left,count);
+            count += cur->left->val;
+            path.pop_back();
+        }
+        if(cur->right)
+        {
+            path.push_back(cur->right->val);
+            count -= cur->right->val;
+            traversal(cur->right,count);
+            count += cur->right->val;
+            path.pop_back();
+        }
+        return;
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        result.clear();
+        path.clear();
+        if(root == NULL) return result;
+        path.push_back(root->val);
+        traversal(root,targetSum - root->val);
+        return result;
+    }
+};
