fb3799c2ea21c21ab7445f191fe0da2f70258f14
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-04-11 22:07:46 +0800

    Create 538. 把二叉搜索树转换为累加树.cpp

---
+++ 538. 把二叉搜索树转换为累加树.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pre = 0;
+    void traversal(TreeNode* cur)
+    {
+        if(cur == nullptr) return;
+        traversal(cur->right);
+        cur->val += pre;
+        pre = cur->val;
+        traversal(cur->left);
+        return;
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        traversal(root);
+        return root;
+    }
+};
