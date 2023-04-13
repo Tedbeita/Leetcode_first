d3221b226682f0700a024948437a39a761609554
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-04-13 21:23:32 +0800

    Create 131. 分割回文串.cpp

---
+++ 131. 分割回文串.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    vector<vector<string>> result;
+    vector<string> path;
+    void backtracking(const string& s,int startIndex)
+    {
+        if(startIndex >= s.size())
+        {
+            result.push_back(path);
+            return;
+        }
+        for(int i = startIndex;i < s.size();i++)
+        {
+            if(isPalindrom(s,startIndex,i))
+            {
+                string str = s.substr(startIndex,i - startIndex + 1);
+                path.push_back(str);
+            }
+            else{
+                continue;
+            }
+            backtracking(s,i+1);
+            path.pop_back();
+        }
+    }
+public:
+    bool isPalindrom(const string& s,int start,int end)
+    {
+        for(int i = start,j = end;i < j;i++,j--)
+        {
+            if(s[i] != s[j]) return false;
+        }
+        return true;
+    }
+    
+    vector<vector<string>> partition(string s) {
+        result.clear();
+        path.clear();
+        backtracking(s,0);
+        return result;
+    }
+};
