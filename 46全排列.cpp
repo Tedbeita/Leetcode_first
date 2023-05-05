60c266fcbb3cea81f607d1fccaa7147d9355b8ed
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-05-05 22:50:32 +0800

    Create 46全排列.cpp

---
+++ 46全排列.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& nums,vector<bool>& used)
+    {
+        if(path.size() == nums.size())
+        {
+            result.push_back(path);
+            return;
+        }
+        for(int i = 0;i < nums.size();i++)
+        {
+            if(used[i] == true) continue;
+            path.push_back(nums[i]);
+            used[i] = true;
+            backtracking(nums,used);
+            used[i] = false;
+            path.pop_back();
+
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        vector<bool> used(nums.size(),false);
+        backtracking(nums,used);
+        return result;
+    }
+};
