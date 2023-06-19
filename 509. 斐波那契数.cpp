6c611477e5896a22fffbfeaecacc2531a18cfb19
TedBeta <104724873+Tedbeita@users.noreply.github.com>
2023-06-19 21:29:16 +0800

    Create 509. 斐波那契数.cpp

---
+++ 509. 斐波那契数.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+        int dp[2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i <=n;i++)
+        {
+            int sum  = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = sum;
+        }
+        return dp[1];
+    }
+};
